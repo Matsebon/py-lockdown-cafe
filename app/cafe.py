14b034a6c0f7f2ce05e617cc667aa5a80de9ed24
Anastasiia Matsebon <anastasiiamatsebon@gmail.com>
2025-04-01 18:19:37 +0200

    completed

--- app/cafe.py
+++ app/cafe.py
@@ -11,11 +11,19 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError
-        expiration_date = visitor["vaccine"].get("expiration_date")
+
+        if "expiration_date" not in visitor["vaccine"]:
+            raise ValueError("Missing expiration date in vaccine information.")
+
+        expiration_date = visitor["vaccine"]["expiration_date"]
+
         if not isinstance(expiration_date, datetime.date):
             raise ValueError("Invalid expiration date format.")
+
         if expiration_date < datetime.date.today():
             raise OutdatedVaccineError
+
         if not visitor.get("wearing_a_mask", False):
             raise NotWearingMaskError
+
         return f"Welcome to {self.name}"
